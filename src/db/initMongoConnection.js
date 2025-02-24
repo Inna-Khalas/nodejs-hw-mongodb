ca56b0b12d3a9b16d2c6b4914a2723731c4a8803
Inna Khalas <innamakarec349@gmail.com>
2025-02-24 17:26:38 +0400

    url

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,7 +9,7 @@ export const initMongoConnection = async () => {
     const db = getEnvVar('MONGODB_DB');
 
     mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0.1gfhk.mongodb.net`,
     );
     console.log('Mongo connection successfully established!');
   } catch (error) {
