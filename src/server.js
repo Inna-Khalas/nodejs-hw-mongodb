3b51be8ae3a5772756483d3d74308b274ccd1dcf
Inna Khalas <innamakarec349@gmail.com>
2025-02-26 11:53:22 +0400

    fix type

--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@ const setUpServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts',
       data: contacts,
     });
@@ -41,10 +42,14 @@ const setUpServer = () => {
       const contact = await getContactById(contactId);
 
       if (!contact) {
-        return res.status(404).json({ message: 'Contact not found' });
+        return res.status(404).json({
+          status: 404,
+          message: 'Contact not found',
+        });
       }
 
       res.status(200).json({
+        status: 200,
         message: `Successfully found contact with id ${contactId}`,
         data: contact,
       });
@@ -55,7 +60,16 @@ const setUpServer = () => {
 
   app.get('*', (req, res) => {
     res.status(404).json({
+      status: 404,
       message: 'Ops',
+    });
+
+    app.use((err, req, res, next) => {
+      res.status(500).json({
+        status: 500,
+        message: 'Something went wrong',
+        error: err.message,
+      });
     });
   });
   app.listen(PORT, () => {
